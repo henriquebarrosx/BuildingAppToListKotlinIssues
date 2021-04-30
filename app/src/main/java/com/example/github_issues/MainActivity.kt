f712456840a30b4ff37e3234cf747cd767397fc5
Henrique Barros <henriquexavierb@gmail.com>
2021-04-30 18:00:21 -0300

    Initialize list on main activity

--- app/src/main/java/com/example/github_issues/MainActivity.kt
+++ app/src/main/java/com/example/github_issues/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.github_issues
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var issueAdapter: IssueAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initAdapterModules()
+    }
+
+    private fun initAdapterModules() {
+        issueAdapter = IssueAdapter(mutableListOf())
+
+        rvIssueItems.adapter = issueAdapter
+        rvIssueItems.layoutManager = LinearLayoutManager(this)
     }
 }
