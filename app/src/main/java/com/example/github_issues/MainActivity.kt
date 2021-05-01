39e4850fd7c6526a2c86313a71db59b479cadcf2
Henrique Barros <henriquexavierb@gmail.com>
2021-04-30 21:23:20 -0300

    Create base promise structure

--- app/src/main/java/com/example/github_issues/MainActivity.kt
+++ app/src/main/java/com/example/github_issues/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.github_issues
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.squareup.okhttp.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var issueAdapter: IssueAdapter
@@ -16,17 +19,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initAdapterModules() {
+        requestIssues()
+
         issueAdapter = IssueAdapter(
             mutableListOf(
                 Issue(
-                    "Creating a new issue",
+                    "This is a opened issue",
                     "open",
-                    "1d"
+                    "13d"
                 ),
                 Issue(
-                "Closing a new issue",
+                    "This is a closed issue",
                     "closed",
-                    "13d"
+                    "1d"
                 )
             )
         )
@@ -34,4 +39,25 @@ class MainActivity : AppCompatActivity() {
         rvIssueItems.adapter = issueAdapter
         rvIssueItems.layoutManager = LinearLayoutManager(this)
     }
+
+    private fun requestIssues() {
+        val promiseURL = "https://api.github.com/repos/JetBrains/kotlin/issues"
+        val promiseBuilder = Request.Builder().url(promiseURL).build()
+        val promise = OkHttpClient()
+
+        val data = mutableListOf<Issue>()
+
+        promise.newCall(promiseBuilder).enqueue(object : Callback {
+            override fun onFailure(request: Request?, e: IOException?) {
+            }
+
+            override fun onResponse(response: Response) {
+            }
+        })
+
+    }
 }
+
+
+
+
